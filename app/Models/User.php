ee9039a6bc712cd18a33fc56b0f7dbf6b070cddb
fauzan-radji <triputrafauzan@gmail.com>
2023-04-06 15:48:10 +0800

    authorize: pembina policy

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,31 +10,56 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $guarded = ['id'];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $guarded = ['id'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  public function pembina()
+  {
+    return $this->hasOne(Pembina::class);
+  }
+
+  public function pikr()
+  {
+    return $this->hasOne(Pikr::class);
+  }
+
+  public function isPembina()
+  {
+    return ($this->pembina) ? true : false;
+  }
+
+  public function isPikr()
+  {
+    return ($this->pikr) ? true : false;
+  }
+
+  public function isAdmin()
+  {
+    return (!($this->isPembina() || $this->isPikr())) ? true : false;
+  }
 }
