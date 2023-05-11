bc89d5e6eb52bdf87d609bdf992a42e87342a5fe
iRumondor <ikirumondor07@gmail.com>
2023-05-11 13:50:46 +0800

    fix: menambah tombol validasi kegiatan di user pembina

--- resources/views/user-pikr/artikel/index.blade.php
+++ resources/views/user-pikr/artikel/index.blade.php
@@ -140,7 +140,7 @@
 
     <div class="modal fade text-left" id="showModal" tabindex="-1" aria-labelledby="" style="display: none;"
         aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="myModalLabel1">
